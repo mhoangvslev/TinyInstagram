2221bd24e2716c1033b67af85ddeb0f618d14685
Minh Hoang DANG <minhhoangdang@hotmail.com>
2019-11-03 17:41:45 +0100

    Ajouter les modèles pour supporter le REST service

---
+++ src/models/post.ts
@@ -0,0 +1,19 @@
+import * as m from "mithril";
+
+export class Post{
+    likes: number
+    shares: number
+
+    constructor(){
+        this.likes = 0;
+        this.shares = 0;
+    }
+
+    likePost(){
+
+    }
+
+    sharePost(){
+
+    }
+}
