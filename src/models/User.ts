550e2896d7a1a832648d7f8ce7f06b2a96e47695
Minh Hoang DANG <minhhoangdang@hotmail.com>
2019-12-02 23:53:05 +0100

    Working examples

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,16 +1,48 @@
-import * as m from "mithril";
-import { Post } from "./Post";
+import m from "mithril";
+import { String } from "typescript-string-operations";
 
 export var User = {
+    userId: null,
+    users: [],
     followers: [],
     following: [],
 
-    getFollowers: function () {
+    /**
+     * Create user
+     */
+
+    /**
+     * Retrieve all users
+     */
+    getUsers: function () {
         return m.request({
             method: "GET",
-            url: "",
-            data: ,
-            withCredentials: true // use cookies?,
+            url: "https://tinyinstagram.appspot.com/_ah/api/tinyinsta/v1/user/all",
+            //data: jsonData,
+            withCredentials: false // use cookies?,
+        }).then(function (result: any) {
+            console.log(typeof (result))
+            User.users = result.items;
+        }).catch((reason: any) => {
+            console.log(reason);
+        })
+    },
+
+    /**
+     * Retrieve all users
+     */
+    getFollowers: function () {
+        const apiURL: string = "https://tinyinstagram.appspot.com/_ah/api/tinyinsta/v1/user/{userId}/followers";
+        return m.request({
+            method: "GET",
+            url: String.Format(apiURL, User.userId),
+            //data: jsonData,
+            withCredentials: false // use cookies?,
+        }).then(function (result: any) {
+            console.log(typeof (result))
+            User.followers = result.items;
+        }).catch((reason: any) => {
+            console.log(reason);
         })
     }
 }
